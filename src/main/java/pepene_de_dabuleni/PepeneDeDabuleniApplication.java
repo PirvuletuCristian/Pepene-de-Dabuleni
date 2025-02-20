2479d465b391ade357e1e1adfd8730e004b84ab3
Pirvuletu Cristian <pirvuletu.cristian13@gmail.com>
2025-02-20 21:10:35 +0200

    Initial commit

---
+++ src/main/java/pepene_de_dabuleni/PepeneDeDabuleniApplication.java
@@ -0,0 +1,13 @@
+package pepene_de_dabuleni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepeneDeDabuleniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepeneDeDabuleniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pepene_de_dabuleni/PepeneDeDabuleniApplicationTests.java
@@ -0,0 +1,13 @@
+package pepene_de_dabuleni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepeneDeDabuleniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
