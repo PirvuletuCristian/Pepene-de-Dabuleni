535f7bbc08f2541b4c95502061105de33a965203
Pirvuletu Cristian <pirvuletu.cristian13@gmail.com>
2026-04-10 10:40:06 +0300

    add delete api

--- backend/src/controllers/producerController.ts
+++ backend/src/controllers/producerController.ts
@@ -1,6 +1,7 @@
 import { pool } from "../config/database.js";
 import { NextFunction, Request, Response } from "express";
 import { logger } from "../utils/logger.js";
+import id from "zod/v4/locales/id.js";
 
 export const createProducer = async (req: Request, res: Response, next: NextFunction) => {
     const { name, product, latitude, longitude } = req.body;
@@ -33,5 +34,33 @@ export const getProducers = async (_: Request, res: Response) => {
     } catch (err) {
         logger.error("Failed to fetch producers:", err);
         res.status(500).json({ error: "Failed to fetch producers" })
+    }
+};
+
+export const deleteProducer = async (req: Request, res: Response, next: NextFunction) => {
+    const producerId = Number(req.params.id);
+
+    //Validate producedID
+    if (!producerId || isNaN(producerId) || producerId <= 0) {
+        return 
+        res.status(400).json({ error: "Invalid producer ID" });
+    }
+
+    try {
+        const result = await pool.query(
+            `DELETE FROM producers WHERE id = $1 RETURNING *`,
+            [producerId]
+        );
+        
+        if (result.rows.length === 0) {
+            logger.warn(`Producer with id ${producerId} not found`);
+            return res.status(404).json({ error: "Producer not found" });
+        }
+        
+        logger.info(`Producer deleted: id ${producerId} (${result.rows[0].name})`);
+        res.status(200).json({ message: "Producer deleted successfully", producer: result.rows[0] });
+    } catch (err) {
+        logger.error("Failed to delete producer:", err);
+        next(err);
     }
 };
--- backend/src/routes/producers.ts
+++ backend/src/routes/producers.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProducer, getProducers } from "../controllers/producerController.js";
+import { createProducer, getProducers, deleteProducer } from "../controllers/producerController.js";
 import { validateProducer } from "../middleware/validation.js";
 
 const router = Router();
 
 router.post("/", validateProducer, createProducer);
 router.get("/", getProducers);
+router.delete("/:id", deleteProducer);
 
 export default router;
